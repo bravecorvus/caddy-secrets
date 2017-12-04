b3526960351f59b4f7406054047adc4c5c680928
Alfredo Uribe <alfredo@cloudspace.com>
2017-12-04 11:56:15 -0500

    first commit

---
+++ secrets.go
@@ -0,0 +1,82 @@
+package secrets
+
+import (
+	//"bytes"
+	"fmt"
+	"github.com/mholt/caddy"
+	"github.com/mholt/caddy/caddyhttp/httpserver"
+	"gopkg.in/yaml.v2"
+	"io/ioutil"
+	"net/http"
+	"os/exec"
+)
+
+func init() {
+	caddy.RegisterPlugin("secrets", caddy.Plugin{
+		ServerType: "http",
+		Action:     setup,
+	})
+}
+
+func setup(c *caddy.Controller) error {
+	for c.Next() {
+		cfg := httpserver.GetConfig(c)
+		mid := func(next httpserver.Handler) httpserver.Handler {
+			return MyHandler{
+				Next: next,
+			}
+		}
+		cfg.AddMiddleware(mid)
+
+		args := c.RemainingArgs()
+		fileName := args[0]
+		secretsUser := args[1]
+		secretsPassword := args[2]
+
+		cmd := exec.Command("/bin/sh", "-c", fmt.Sprintf("echo %s | sudo -S chown `whoami` %s", secretsPassword, fileName))
+		cmd.Run()
+
+		// var out bytes.Buffer
+		// var stderr bytes.Buffer
+		// cmd.Stdout = &out
+		// cmd.Stderr = &stderr
+		// err := cmd.Run()
+		// if err != nil {
+		// 	fmt.Printf("err: %s\n", stderr.String())
+		// } else {
+		// 	fmt.Println(out.String())
+		// }
+
+		content, err := ioutil.ReadFile(fileName)
+		if err != nil {
+			fmt.Println(err.Error())
+		} else {
+
+			m := yaml.MapSlice{}
+			err := yaml.Unmarshal([]byte(content), &m)
+			if err != nil {
+				fmt.Printf("error: %v\n", err.Error())
+			} else {
+				fmt.Printf("yaml: %v\n", m)
+			}
+
+			cmd = exec.Command("/bin/sh", "-c", fmt.Sprintf("sudo chown %s %s", secretsUser, fileName))
+			cmd.Run()
+		}
+
+		mids := cfg.Middleware()
+		secrets := mids[0](httpserver.EmptyNext)
+		fmt.Printf("\n%T %v\n", secrets, secrets)
+	}
+
+	return nil
+}
+
+type MyHandler struct {
+	Next       httpserver.Handler
+	SecretsMap map[string]string
+}
+
+func (h MyHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) (int, error) {
+	return h.Next.ServeHTTP(w, r)
+}
