9b9bab003b3ac3c19ccd72af4db113796853fba2
Alfredo Uribe <alfredo@cloudspace.com>
2017-12-07 20:03:40 -0500

    first refactor

--- secrets.go
+++ secrets.go
@@ -1,14 +1,12 @@
 package secrets
 
 import (
-	//"bytes"
-	"fmt"
+	"errors"
 	"github.com/mholt/caddy"
 	"github.com/mholt/caddy/caddyhttp/httpserver"
 	"gopkg.in/yaml.v2"
 	"io/ioutil"
 	"net/http"
-	"os/exec"
 )
 
 func init() {
@@ -18,63 +16,48 @@ func init() {
 	})
 }
 
+type MyHandler struct {
+	Next httpserver.Handler
+}
+
+var SecretsMap *yaml.MapSlice
+
 func setup(c *caddy.Controller) error {
-	for c.Next() {
-		args := c.RemainingArgs()
-		fileName := args[0]
-		secretsUser := args[1]
-		secretsPassword := args[2]
+	if c.Next() {
+		c.Next()
 
-		cmd := exec.Command("/bin/sh", "-c", fmt.Sprintf("echo %s | sudo -S chown `whoami` %s", secretsPassword, fileName))
-		cmd.Run()
-
-		// var out bytes.Buffer
-		// var stderr bytes.Buffer
-		// cmd.Stdout = &out
-		// cmd.Stderr = &stderr
-		// err := cmd.Run()
-		// if err != nil {
-		// 	fmt.Printf("err: %s\n", stderr.String())
-		// } else {
-		// 	fmt.Println(out.String())
-		// }
-
-		m := yaml.MapSlice{}
-		content, err := ioutil.ReadFile(fileName)
-		if err != nil {
-			fmt.Println(err.Error())
-		} else {
-
-			err := yaml.Unmarshal([]byte(content), &m)
-			if err != nil {
-				fmt.Printf("error: %v\n", err.Error())
-			} else {
-				fmt.Printf("yaml: %v\n", m)
-			}
-			cmd = exec.Command("/bin/sh", "-c", fmt.Sprintf("sudo chown %s %s", secretsUser, fileName))
-			cmd.Run()
+		fileName := c.Val()
+		if err := readFile(fileName); err != nil {
+			return err
 		}
 
 		cfg := httpserver.GetConfig(c)
 		mid := func(next httpserver.Handler) httpserver.Handler {
 			return MyHandler{
-				SecretsMap: m,
-				Next:       next,
+				Next: next,
 			}
 		}
 		cfg.AddMiddleware(mid)
 
-		// mids := cfg.Middleware()
-		// secrets := mids[0](httpserver.EmptyNext)
-		// fmt.Printf("\n%T %v\n", secrets, secrets)
+		if c.Next() {
+			return errors.New("Secrets middleware received more arguments than expected")
+		}
 	}
-
 	return nil
 }
 
-type MyHandler struct {
-	Next       httpserver.Handler
-	SecretsMap yaml.MapSlice
+func readFile(fileName string) error {
+	m := yaml.MapSlice{}
+	if content, err := ioutil.ReadFile(fileName); err != nil {
+		return err
+
+	} else {
+		if err = yaml.Unmarshal([]byte(content), &m); err != nil {
+			return err
+		}
+		SecretsMap = &m
+	}
+	return nil
 }
 
 func (h MyHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) (int, error) {
